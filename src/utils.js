1b273922779aa55b9f0883220068cf0c28e92719
pabloInaipil <pablo.inaipil@gmail.com>
2021-09-21 17:56:40 -0300

    fix the shit

--- src/utils.js
+++ src/utils.js
@@ -9,11 +9,10 @@ export const createImageNodes = () => {
 
     const image = document.createElement("img");
     image.className = "mx-auto rounded-md bg-gray-300";
-    image.width = 320; 
     image.src = 
     "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=";
     image.dataset.src = `https://randomfox.ca/images/${random()}.jpg`; 
-
+    image.width = 320; 
    wrapper.append(image);
 
    return [wrapper, image];
