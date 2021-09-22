2555f672252abf53d8c9e19f9b24d1f0c615334e
pabloInaipil <pablo.inaipil@gmail.com>
2021-09-22 11:51:58 -0300

    commit13

--- src/lazy.js
+++ src/lazy.js
@@ -1,35 +1,33 @@
-
 let totalImages = 0;
 let loadedImages = 0;
-
-const observer = new IntersectionObserver((entries) => { 
+//Instanciar Intersection Observer
+const observer = new IntersectionObserver((entries) => {
+    //esto es lo que va a ejecutar el observer por cada elemento que observa
     entries.filter(isIntersecting).forEach(loadImage);
-
 });
-
 
 const isIntersecting = (intersectionEntry) => intersectionEntry.isIntersecting;
 
-const loadImage = (intersectionEntry) => { 
-    const imgNode = intersectionEntry.target; // container (DIV)
-    imgNode.src = imgNode.dataset.src;
-    imgNode.onload = () => { 
+const loadImage = (intersectionEntry) => {
+    const imgNode = intersectionEntry.target;//imagen 
+    imgNode.src = imgNode.dataset.url_imagen;
+    //incrementa la cantidad de la variable loadedImages
+    imgNode.onload = () => {
         loadedImages += 1;
         logState();
     };
-    // des registra la imagen (unlisten)
-     observer.unobserve(imgNode); 
+    // desregistra o deja de observar la imagen
+    observer.unobserve(imgNode);
 };
-
-
-export const registerImage = (image) => {
-    observer.observe(image);
+//Crear funcion
+export const registerImage = (imagen)=>{
+    observer.observe(imagen)
     totalImages += 1;
     logState();
 };
 
-function logState() { 
-    console.log(`Total Imágenes: ${totalImages}`);
-    console.log(`Imágenes cargadas: ${loadedImages}`);
-    console.log("-----------------------------------");
-}
+function logState() {
+    console.log(`✳ Total imágenes: ${totalImages}`);
+    console.log(`✅ Imágenes cargadas: ${loadedImages}`);
+    console.log('----------------------------------------');
+};
